dd0a059f2c05e4535f867b136677bbcce721029f
masal9pse <returnymgstokh@icloud.com>
2022-05-14 15:01:42 +0900

    first commit

---
+++ FirstSwiftUIProject/FirstSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIProjectApp.swift
+//  FirstSwiftUIProject
+//
+//  Created by 山本大翔 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
